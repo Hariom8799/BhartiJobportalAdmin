0b48c76951222f11df8654c63617ded7cdf7cf4b
hariom8799 <hariomkumar01868@gmail.com>
2025-06-04 15:27:21 +0530

    fix : change the logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,8 +3,13 @@ import ThemeProvider from "@/Context/ThemeContext";
 import { Toaster } from "react-hot-toast";
 
 export const metadata = {
-  title: "Next Js Dashboard",
-  description: "Next Js Dashboard Tutorial",
+  title: "Bharti Job Portal - Admin",
+  description: "Admin Dashboard for Bharti Job Portal Uttarakhand",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon-32x32.png",
+  },
 };
 
 export default function RootLayout({ children }) {
